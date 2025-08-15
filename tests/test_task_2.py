4ecad7f61b1538790eecdad598bcca0274968172
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-15 22:59:42 +0300

    Task 2 tests fixed

--- tests/test_task_2.py
+++ tests/test_task_2.py
@@ -3,17 +3,29 @@ import pytest
 from tasks.task_2 import odometer
 
 
+def test_regression():
+    test_arr = [20, 2, 30, 6, 10, 7]
+    test_res = 170
+    assert odometer(test_arr) == test_res
+
+
 @pytest.mark.repeat(100)
-def test_regression_and_random():
+def test_random():
     random_n = random.randint(1, 100)
     test_res = 0
+    hours_prev = 0
     test_arr = []
     for _ in range(random_n):
-        for j in range(2):
-            num = random.randint(1, 50)
-            if j == 0:
-                test_res += num
-            test_arr.append(num)
+        random_speed = random.randint(1, 100)
+        test_arr.append(random_speed)
+        random_hours = random.randint(hours_prev, 10)
+        test_arr.append(random_hours)
+        test_res += (random_hours - hours_prev) * random_speed
+        hours_prev = random_hours
+
+        if random_hours == 10:
+            break
+
     assert odometer(test_arr) == test_res
 
 
@@ -23,6 +35,6 @@ def test_min_max():
     test_res_min = 0
     test_res_max = 10**20
     test_res_min = arr_min[0]
-    test_res_max = arr_max[0]
+    test_res_max = arr_max[0] * arr_max[1]
     assert odometer(arr_min) == test_res_min
     assert odometer(arr_max) == test_res_max
