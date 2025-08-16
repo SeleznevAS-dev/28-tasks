a504edd06d0b218322f113ddfc8e44b0c2f118ef
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-16 15:40:25 +0300

    Task 3 tests modified

--- tests/test_task_3.py
+++ tests/test_task_3.py
@@ -4,8 +4,10 @@ from tasks.task_3 import ConquestCampaign
 def test_regression():
     test_N = 3
     test_M = 4
-    test_L = 2
-    test_battalion = [2, 2, 3, 4]
+    test_L1 = 2
+    test_battalion1 = [2, 2, 3, 4]
+    test_L2 = 3
+    test_battalion2 = [2, 2, 2, 2, 3, 4]
     test_res = 3
-    assert ConquestCampaign(test_N, test_M, test_L, test_battalion) == test_res
-
+    assert ConquestCampaign(test_N, test_M, test_L1, test_battalion1) == test_res
+    assert ConquestCampaign(test_N, test_M, test_L2, test_battalion2) == test_res
