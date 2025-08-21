2a96925ca2a1810cba0ff9436dbdc6c1ff09c221
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-21 20:13:29 +0300

    Added one more test for task 7

--- tests/test_task_7.py
+++ tests/test_task_7.py
@@ -8,3 +8,10 @@ def test_regression():
     )
     test_subs = "строк"
     assert WordSearch(test_width, test_s, test_subs) == [0, 0, 0, 1, 0, 0, 0]
+
+
+def test_regression2():
+    test_width = 3
+    test_s = "12345"
+    test_subs = "123"
+    assert WordSearch(test_width, test_s, test_subs) == [1, 0]
