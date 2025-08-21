ab0db5b78db1d55ffc24c8666c0799d1b4cd2b98
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-21 22:28:19 +0300

    Task 8 solved

---
+++ tasks/task_8.py
@@ -0,0 +1,10 @@
+def SumOfThe(N: int, data: list[int]) -> int:
+    for i in range(N):
+        potential_sums = data.copy()
+        potential_sum = potential_sums.pop(i)
+        sm = 0
+        for j in range(N - 1):
+            sm += potential_sums[j]
+        if sm == potential_sum:
+            return sm
+
