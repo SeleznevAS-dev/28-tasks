614f039387903fedc06246d90cbf453b9c1ff818
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-21 20:13:18 +0300

    Fixed task 7

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,46 +1,45 @@
 def WordSearch(width: int, s: str, subs: str) -> list[int]:
     # Part 1
     strings = []
-    length_s = len(s)
     free_space = width
-    i_start = 0
-    i_end = 0
-    i_old = 0
     length = 0
+    i_start = 0
+    i_last = 0
     previous_string = ""
+    s += " "
+    length_s = len(s)
     for i in range(length_s):
-        if s[i] == " " or i == length_s - 1:
+        if s[i] == " ":
             if length <= free_space:
-                i_end = i
                 if len(strings) != 0:
-                    previous_string = strings.pop()
-                new_string = previous_string + "".join(s[i_old:i_end])
+                    previous_string = (strings.pop()) + " "
+                new_string = previous_string + "".join(s[i_start:i_last])
                 strings.append(new_string)
-                free_space -= i_end - i_old
+                free_space -= i - i_start + 1
             else:
-                if length > width:
-                    for k in range((length // width) + 1):
-                        i_start = i_old + 1
-                        if k != (length // width):
-                            i_start = i_old + 1
-                            i_end = i_old + width
-                        else:
-                            i_end = i
-                        new_string = "".join(s[i_start:i_end])
-                        strings.append(new_string)
-                        free_space = width
-                        free_space -= i - i_old + 1
-                        i_old = i_end
-                else:
+                if length <= width:
                     free_space = width
-                    free_space -= i - i_old
-                    i_start = i_old + 1
-                    i_end = i if i < length_s - 1 else i + 1
-                    new_string = s[i_old + 1 : i_end]
+                    new_string = s[i_start:i_last]
                     strings.append(new_string)
-            i_old = i
+                    free_space -= i - i_start + 1
+                else:
+                    cycles = length // width
+                    for k in range(cycles + 1):
+                        free_space = width
+                        if k < cycles:
+                            i_last = i_start + width
+                        else:
+                            i_last = i
+                        new_string = "".join(s[i_start:i_last])
+                        strings.append(new_string)
+                        free_space -= i - i_last + 1
+                        i_start = i_last
+                        assert len(strings[-1]) <= width
+            i_start = i + 1
+            assert len(strings[-1]) <= width
             length = 0
         else:
+            i_last = i + 1
             length += 1
 
     # Part 2
