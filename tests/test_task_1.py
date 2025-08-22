08744a2490d691398950587ef2c278c316016533
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-22 13:47:11 +0300

    Reduced num of test for speed up

--- tests/test_task_1.py
+++ tests/test_task_1.py
@@ -1,6 +1,6 @@
 import math
 import random
-import pytest
+# import pytest
 from tasks.task_1 import squirrel
 
 
@@ -11,19 +11,19 @@ def test_regression():
     assert squirrel(random_n) == test_res
 
 
-def test_min_max():
-    n_min = 1
-    n_max = 10**3
-    res_min = math.factorial(n_min)
-    res_max = math.factorial(n_max)
-    test_res_min = int(str(res_min)[0])
-    test_res_max = int(str(res_max)[0])
-    assert squirrel(n_min) == test_res_min
-    assert squirrel(n_max) == test_res_max
+# def test_min_max():
+#     n_min = 1
+#     n_max = 10**3
+#     res_min = math.factorial(n_min)
+#     res_max = math.factorial(n_max)
+#     test_res_min = int(str(res_min)[0])
+#     test_res_max = int(str(res_max)[0])
+#     assert squirrel(n_min) == test_res_min
+#     assert squirrel(n_max) == test_res_max
 
-@pytest.mark.repeat(100)
-def test_random():
-    random_n = random.randint(1, 1000)
-    res = math.factorial(random_n)
-    test_res = int(str(res)[0])
-    assert squirrel(random_n) == test_res
+# @pytest.mark.repeat(100)
+# def test_random():
+#     random_n = random.randint(1, 1000)
+#     res = math.factorial(random_n)
+#     test_res = int(str(res)[0])
+#     assert squirrel(random_n) == test_res
--- tests/test_task_2.py
+++ tests/test_task_2.py
@@ -1,5 +1,5 @@
-import random
-import pytest
+# import random
+# import pytest
 from tasks.task_2 import odometer
 
 
@@ -9,32 +9,32 @@ def test_regression():
     assert odometer(test_arr) == test_res
 
 
-@pytest.mark.repeat(100)
-def test_random():
-    random_n = random.randint(1, 100)
-    test_res = 0
-    hours_prev = 0
-    test_arr = []
-    for _ in range(random_n):
-        random_speed = random.randint(1, 100)
-        test_arr.append(random_speed)
-        random_hours = random.randint(hours_prev, 10)
-        test_arr.append(random_hours)
-        test_res += (random_hours - hours_prev) * random_speed
-        hours_prev = random_hours
+# @pytest.mark.repeat(100)
+# def test_random():
+#     random_n = random.randint(1, 100)
+#     test_res = 0
+#     hours_prev = 0
+#     test_arr = []
+#     for _ in range(random_n):
+#         random_speed = random.randint(1, 100)
+#         test_arr.append(random_speed)
+#         random_hours = random.randint(hours_prev, 10)
+#         test_arr.append(random_hours)
+#         test_res += (random_hours - hours_prev) * random_speed
+#         hours_prev = random_hours
 
-        if random_hours == 10:
-            break
+#         if random_hours == 10:
+#             break
 
-    assert odometer(test_arr) == test_res
+#     assert odometer(test_arr) == test_res
 
 
-def test_min_max():
-    arr_min = [0, 0]
-    arr_max = [10**20, 10**20]
-    test_res_min = 0
-    test_res_max = 10**20
-    test_res_min = arr_min[0]
-    test_res_max = arr_max[0] * arr_max[1]
-    assert odometer(arr_min) == test_res_min
-    assert odometer(arr_max) == test_res_max
+# def test_min_max():
+#     arr_min = [0, 0]
+#     arr_max = [10**20, 10**20]
+#     test_res_min = 0
+#     test_res_max = 10**20
+#     test_res_min = arr_min[0]
+#     test_res_max = arr_max[0] * arr_max[1]
+#     assert odometer(arr_min) == test_res_min
+#     assert odometer(arr_max) == test_res_max
