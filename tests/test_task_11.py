f69b9900614bdbbf5726707f8b9607bb008c9e67
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-26 19:58:53 +0300

    Added tests for task 11

---
+++ tests/test_task_11.py
@@ -0,0 +1,23 @@
+from tasks.task_11 import BigMinus
+import pytest
+from random import randint
+
+
+def test_regression():
+    test_s1 = "1234567891"
+    test_s2 = "1"
+    assert BigMinus(test_s1, test_s2) == "1234567890"
+
+
+def test_regression2():
+    test_s1 = "1"
+    test_s2 = "321"
+    assert BigMinus(test_s1, test_s2) == "320"
+
+
+@pytest.mark.repeat(1000)
+def test_random():
+    test_s1 = str(randint(1, 1000))
+    test_s2 = str(randint(1, 1000))
+    print(test_s1, test_s2)
+    assert BigMinus(test_s1, test_s2) == str(abs(int(test_s1) - int(test_s2)))
