4afd286093fe7c314d17141fdc5bf28fa6e022c7
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-08-26 21:34:22 +0300

    Added test for task 12

---
+++ tests/test_task_12.py
@@ -0,0 +1,19 @@
+from tasks.task_12 import MassVote
+
+
+def test_regression():
+    test_N = 5
+    test_Votes = [60, 10, 10, 15, 5]
+    assert MassVote(test_N, test_Votes) == "majority winner 1"
+
+
+def test_regression2():
+    test_N = 3
+    test_Votes = [10, 15, 10]
+    assert MassVote(test_N, test_Votes) == "minority winner 2"
+
+
+def test_regression3():
+    test_N = 4
+    test_Votes = [111, 111, 110, 110]
+    assert MassVote(test_N, test_Votes) == "no winner"
