20eac496b6bc8ed2653be0ddc88534247801981e
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-02 17:58:48 +0300

    Added one more test for task 14

--- tests/test_task_14.py
+++ tests/test_task_14.py
@@ -6,3 +6,9 @@ def test_regression():
     test_N = 2
     tesk_track = [[3, 5, 5], [5, 2, 2]]
     assert Unmanned(test_L, test_N, tesk_track) == 12
+
+def test_regression2():
+    test_L = 10
+    test_N = 2
+    tesk_track = [[11, 5, 5], [15, 2, 2]]
+    assert Unmanned(test_L, test_N, tesk_track) == 10
