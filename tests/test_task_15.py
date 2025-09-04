ab86eef71e81949590fe36a588f2f6379b248da4
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-05 00:26:03 +0300

    Added one more test for task 15

--- tests/test_task_15.py
+++ tests/test_task_15.py
@@ -79,3 +79,12 @@ def test_regression8():
     test_W2 = 2
     test_S2 = "11 11"
     assert TankRush(test_H1, test_W1, test_S1, test_H2, test_W2, test_S2) is True
+    
+def test_regression9():
+    test_H1 = 5
+    test_W1 = 15
+    test_S1 = "000000000000000 000000000000000 000000100000000 000000000000000 000000000000000"
+    test_H2 = 3
+    test_W2 = 5
+    test_S2 = "00000 00000 00001"
+    assert TankRush(test_H1, test_W1, test_S1, test_H2, test_W2, test_S2) is True
