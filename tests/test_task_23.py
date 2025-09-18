ade5d8c4b24ebbde8c985982a072fa90297be29f
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-18 21:33:09 +0300

    Added more tests for task 23

--- tests/test_task_23.py
+++ tests/test_task_23.py
@@ -3,3 +3,5 @@ from tasks.task_23 import TreeOfLife
 
 def test_regression():
     assert TreeOfLife(3, 4, 12, [".+..", "..+.", ".+.."]) == [".+..", "..+.", ".+.."]
+    assert TreeOfLife(3, 4, 1, [".+..", "..+.", ".+.."]) == ["++++", "++++", "++++"]
+    assert TreeOfLife(3, 3, 2, ["+++", "+++", "+++"]) == ["...", "...", "..."]
