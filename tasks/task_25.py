dfb12779f231a25051f02e0f380e6d2555efb186
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-20 23:21:38 +0300

    Task 25 solved

---
+++ tasks/task_25.py
@@ -0,0 +1,13 @@
+def Transform(A: list[int], N: int) -> list:
+    B = []
+    for i in range(N):
+        for j in range(N - i):
+            k = i + j
+            if A[j:k] != []:
+                B.append(max(A[j:k]))
+    return B
+
+
+def TransformTransform(A: list[int], N: int) -> bool:
+    B = Transform(A, N)
+    return bool(sum(Transform(B, len(B))) % 2 == 0)
