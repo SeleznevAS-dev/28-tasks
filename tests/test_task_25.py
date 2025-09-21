d3a04b1a7f2a7eda997ebee1430ee0dac46bebf8
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-21 11:06:41 +0300

    Fixed tests for task 25

--- tests/test_task_25.py
+++ tests/test_task_25.py
@@ -2,5 +2,6 @@ from tasks.task_25 import TransformTransform
 
 
 def test_regression():
-    assert TransformTransform([1, 2, 3], 3) is False
-    assert TransformTransform([1, 2, 3, 4], 4) is True
+    assert TransformTransform([1, 2, 3], 3) is True
+    assert TransformTransform([1, 2, 3, 4], 4) is False
+    assert TransformTransform([1, 2, 1, 7, 2, 4, 3, 1, 5, 1, 2, 1, 6, 1, 2], 15) is False
