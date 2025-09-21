41156feefc06a3bbd70ef7f9e66bb9996b21f944
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-21 11:07:55 +0300

    Fixed ident

--- tests/test_task_25.py
+++ tests/test_task_25.py
@@ -5,3 +5,4 @@ def test_regression():
     assert TransformTransform([1, 2, 3], 3) is True
     assert TransformTransform([1, 2, 3, 4], 4) is False
     assert TransformTransform([1, 2, 1, 7, 2, 4, 3, 1, 5, 1, 2, 1, 6, 1, 2], 15) is False
+    
