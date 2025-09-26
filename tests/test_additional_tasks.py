64c7b2090b69221ca367447feba9d8d6b5beadfe
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-26 13:24:10 +0300

    Added tests for additional tasks 3 and 4

--- tests/test_additional_tasks.py
+++ tests/test_additional_tasks.py
@@ -1,6 +1,8 @@
 from tasks.additional_tasks import factorial
 from tasks.additional_tasks import power
 from tasks.additional_tasks import sum_numbers
+from tasks.additional_tasks import list_len
+from tasks.additional_tasks import is_palindrome
 
 
 def test_regression_0():
@@ -16,3 +18,15 @@ def test_regression_1():
 def test_regression_2():
     assert sum_numbers(12) == 3
     assert sum_numbers(9999) == 36
+
+
+def test_regression_3():
+    assert list_len([1]) == 1
+    assert list_len([1, 1, 1, 1, 1]) == 5
+
+
+def test_regression_4():
+    assert is_palindrome("aba") is True
+    assert is_palindrome("abc") is False
+    assert is_palindrome("abccba") is True
+    assert is_palindrome("abccbb") is False
