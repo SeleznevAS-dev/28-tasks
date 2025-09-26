11f8872e9c39d434a0c25f087578e315977bec81
SeleznevAS-dev <its0a0magic0time@gmail.com>
2025-09-26 13:23:52 +0300

    Additional tasks 3 and 4 solved

--- tasks/additional_tasks.py
+++ tasks/additional_tasks.py
@@ -17,3 +17,25 @@ def sum_numbers(N: int) -> int:
     if len(str(N)) == 1:
         return N % 10
     return N % 10 + sum_numbers(N // 10)
+
+
+# 3.
+def list_len(arr: list) -> int:
+    if len(arr) == 1:
+        return 1
+    arr.pop()
+    return 1 + list_len(arr)
+
+
+# 4.
+def is_palindrome(string: str) -> bool:
+    if len(string) > 2 and not is_palindrome(string[1:-1]):
+        return False
+
+    ans = False
+    if (string[0] == string[-1]) or len(string) == 1:
+        ans = True
+    elif string[0] != string[-1]:
+        ans = False
+
+    return ans
